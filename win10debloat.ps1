5f4a4a2886bb6d65931f5609bda366e4c397ac77
Paul <29339012+Xarchuser@users.noreply.github.com>
2022-01-15 19:02:17 +0000

    Added Foxit Reader

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -84,12 +84,12 @@ $sharex.height                   = 30
 $sharex.location                 = New-Object System.Drawing.Point(3,561)
 $sharex.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$adobereader                     = New-Object system.Windows.Forms.Button
-$adobereader.text                = "Adobe Reader DC"
-$adobereader.width               = 212
-$adobereader.height              = 30
-$adobereader.location            = New-Object System.Drawing.Point(3,865)
-$adobereader.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
+$foxit 			         = New-Object system.Windows.Forms.Button
+$foxit.text                      = "Foxit Reader"
+$foxit.width                     = 212
+$foxit.height                    = 30
+$foxit.location                  = New-Object System.Drawing.Point(3,865)
+$foxit.Font                      = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $notepad                         = New-Object system.Windows.Forms.Button
 $notepad.text                    = "Notepad++"
@@ -620,7 +620,7 @@ $Button1.location                = New-Object System.Drawing.Point(4,197)
 $Button1.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
 $Form.controls.AddRange(@($Panel1,$Panel2,$Label3,$Label15,$Panel4,$PictureBox1,$Label1,$Label4,$Panel3,$ResultText,$Label10,$Label11,$urlfixwinstartup,$urlremovevirus,$urlcreateiso))
-$Panel1.controls.AddRange(@($librewolf,$firefox,$7zip,$sharex,$adobereader,$notepad,$gchrome,$mpc,$vlc,$powertoys,$winterminal,$vscode,$Label2,$everythingsearch,$sumatrapdf,$vscodium,$imageglass,$gimp,$Label7,$Label8,$Label9,$advancedipscanner,$putty,$etcher,$translucenttb,$githubdesktop,$discord,$autohotkey))
+$Panel1.controls.AddRange(@($librewolf,$firefox,$7zip,$sharex,$foxit,$notepad,$gchrome,$mpc,$vlc,$powertoys,$winterminal,$vscode,$Label2,$everythingsearch,$sumatrapdf,$vscodium,$imageglass,$gimp,$Label7,$Label8,$Label9,$advancedipscanner,$putty,$etcher,$translucenttb,$githubdesktop,$discord,$autohotkey))
 $Panel2.controls.AddRange(@($essentialtweaks,$backgroundapps,$cortana,$actioncenter,$darkmode,$performancefx,$onedrive,$lightmode,$essentialundo,$EActionCenter,$ECortana,$RBackgroundApps,$HTrayIcons,$EClipboardHistory,$ELocation,$InstallOneDrive,$removebloat,$reinstallbloat,$WarningLabel,$Label5,$appearancefx,$STrayIcons,$EHibernation,$dualboottime))
 $Panel4.controls.AddRange(@($defaultwindowsupdate,$securitywindowsupdate,$Label16,$Label17,$Label18,$Label19))
 $Panel3.controls.AddRange(@($yourphonefix,$Label6,$windowsupdatefix,$ncpa,$oldcontrolpanel,$oldsoundpanel,$Button1))
@@ -671,12 +671,12 @@ $discord.Add_Click({
     $ResultText.text = "`r`n" + "Finished Installing Discord" + "`r`n" + "`r`n" + "Ready for Next Task"
 })
 
-$adobereader.Add_Click({
-    Write-Host "Installing Adobe Reader DC"
-    $ResultText.text = "`r`n" +"`r`n" + "Installing Adobe Reader DC... Please Wait" 
-    winget install -e Adobe.AdobeAcrobatReaderDC | Out-Host
-    if($?) { Write-Host "Installed Adobe Reader DC" }
-    $ResultText.text = "`r`n" + "Finished Installing Adobe Reader DC" + "`r`n" + "`r`n" + "Ready for Next Task"
+$foxit.Add_Click({
+    Write-Host "Installing Foxit Reader"
+    $ResultText.text = "`r`n" +"`r`n" + "Installing Foxit Reader... Please Wait" 
+    winget install -e Foxit.FoxitReader| Out-Host
+    if($?) { Write-Host "Installed Foxit Reader" }
+    $ResultText.text = "`r`n" + "Finished Installing Foxit Reader" + "`r`n" + "`r`n" + "Ready for Next Task"
 })
 
 $notepad.Add_Click({
